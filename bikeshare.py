59433d1e14f913a70c94621e201e4c76e6aea517
ayako kurahashi <ayakokurahashi@hotmail.com>
2021-05-27 22:18:47 +0900

    Deleted Print ("some") on def get_filters()

--- bikeshare.py
+++ bikeshare.py
@@ -22,7 +22,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print("Hello! Let's explore some US bikeshare data!")
+    print("Hello! Let's explore US bikeshare data!")
 
     # Ask users to input city (chicago, new york city, or washington)
     city_name = " "
